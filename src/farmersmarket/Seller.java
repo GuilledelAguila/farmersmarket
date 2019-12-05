66ee5f905d659195aaef3527f5018940173c483d
nahso <harper.n@husky.neu.edu>
2019-12-05 15:56:11 -0500

    seller tables

--- src/farmersmarket/Seller.java
+++ src/farmersmarket/Seller.java
@@ -29,6 +29,38 @@ public class Seller {
 		seller.setSize((int)screenWidth / 3, (int)screenHeight - 500);
 		
 		seller.setLocation((int)screenWidth / 3, 0);
+		
+		// creates table displaying all produce available in the catalog
+		GetTable catalog = new GetTable(conn);
+    JTable catalogTable = catalog.runTable("select * from catalog");
+    
+        JScrollPane scrollPaneCatalog = new JScrollPane(catalogTable);
+        catalogTable.setFillsViewportHeight(true);
+ 
+        JLabel lblHeadingCatalog = new JLabel("Produce Available to Sell");
+      
+        seller.getContentPane().setLayout(new BorderLayout());
+        
+        seller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+     // creates table displaying all farmers available to partner with
+        GetTable farmers = new GetTable(conn);
+        JTable farmersTable = farmers.runTable("select * from farm");
+        
+            JScrollPane scrollPaneFarmers = new JScrollPane(farmersTable);
+            farmersTable.setFillsViewportHeight(true);
+     
+            JLabel lblHeadingFarmers = new JLabel("Farms Available to Partner With");
+     
+            seller.getContentPane().setLayout(new BorderLayout());
+            /*
+            seller.add(lblHeadingFarmers,BorderLayout.PAGE_START);
+            seller.add(scrollPaneCatalog, BorderLayout.CENTER);
+            seller.add(lblHeadingCatalog,BorderLayout.SOUTH);
+            seller.add(scrollPaneFarmers, BorderLayout.SOUTH);
+            seller.validate();
+            */
+            seller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
         
 		seller.setVisible(true);
